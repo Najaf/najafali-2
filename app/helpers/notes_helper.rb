36df4204e02e12134ed7f59caffaf39e3b467b76
Najaf Ali <ali.najaf@gmail.com>
2023-04-02 21:00:06 +0900

    Adds TOC to notes

---
+++ app/helpers/notes_helper.rb
@@ -0,0 +1,37 @@
+module NotesHelper
+  def markdown_renderer
+    Redcarpet::Render::HTML.new({ with_toc_data: true })
+  end
+
+  def markdown
+    Redcarpet::Markdown.new(markdown_renderer,
+      {fenced_cod_blocks: true}
+    )
+  end
+
+  def toc_renderer
+    Redcarpet::Markdown.new(
+      Redcarpet::Render::HTML_TOC
+    )
+  end
+
+  def show_contents?
+    word_count > 1000 && table_of_contents.strip.present?
+  end
+
+  def rendered_body
+    markdown.render(@note.body).html_safe
+  end
+
+  def word_count
+    rendered_body.split(' ').count
+  end
+
+  def table_of_contents
+    toc_renderer.render(@note.body).html_safe
+  end
+
+  def publish_date
+    @note.publish_date.strftime("%B %Y")
+  end
+end
